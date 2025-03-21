8d34c5304a4f4aa970f81935866285c0c72aa8b1
Daniel Skowroński <daniel@skowron.ski>
2025-03-21 02:07:10 +0100

    WiFi, NTP, TimeZone, ISO8601 WeekNum

--- code/LIBRARIES.md
+++ code/LIBRARIES.md
@@ -2,3 +2,4 @@
 
 - [u8g2](https://github.com/olikraus/u8g2) - 2.25.30
   - https://github.com/olikraus/u8g2/wiki/fntgrpspleen
+- [AceTime](https://github.com/bxparks/AceTime) - 2.4.0
--- code/UTCMON/UTCMON.ino
+++ code/UTCMON/UTCMON.ino
@@ -1,10 +1,59 @@
 #include <SPI.h>
 #include <U8g2lib.h>
+#include <WiFi.h>
+#include <AceTime.h>
+#include <time.h>
+#include "Secrets.h"
+
+using namespace ace_time;
+static const int CACHE_SIZE = 3;
+static ExtendedZoneProcessorCache<CACHE_SIZE> zoneProcessorCache;
+static ExtendedZoneManager manager(
+    zonedbx::kZoneAndLinkRegistrySize,
+    zonedbx::kZoneAndLinkRegistry,
+    zoneProcessorCache);
 
 U8G2_SSD1322_NHD_256X64_F_4W_HW_SPI u8g2_l(U8G2_R0, /* cs=*/ 5, /* dc=*/ 0, /* reset=*/ 4);
 U8G2_SSD1322_NHD_256X64_F_4W_HW_SPI u8g2_r(U8G2_R0, /* cs=*/ 17, /* dc=*/ 2, /* reset=*/ 16);
 
-void drawClock(int year, int month, int day, int week, char* weekday, int hour, int minute, int second, char* timezone){
+int computeIsoWeekNumber(int year, int month, int day) {
+  // Build a tm structure for the given date at noon to avoid DST edge issues.
+  struct tm tmDate = {0};
+  tmDate.tm_year = year - 1900;
+  tmDate.tm_mon  = month - 1;
+  tmDate.tm_mday = day;
+  tmDate.tm_hour = 12; 
+  mktime(&tmDate);
+  
+  // Determine ISO weekday (Monday=1, Tuesday=2, …, Sunday=7)
+  int isoWeekday = (tmDate.tm_wday == 0) ? 7 : tmDate.tm_wday;
+  
+  // Shift the date to the Thursday of the current week.
+  time_t t = mktime(&tmDate);
+  time_t t_thursday = t + (4 - isoWeekday) * 86400; // 86400 seconds per day
+  struct tm tmThursday;
+  localtime_r(&t_thursday, &tmThursday);
+  int isoYear = tmThursday.tm_year + 1900;
+  
+  // Compute January 4th of the ISO year.
+  struct tm tmJan4 = {0};
+  tmJan4.tm_year = isoYear - 1900;
+  tmJan4.tm_mon  = 0; // January
+  tmJan4.tm_mday = 4;
+  tmJan4.tm_hour = 12;
+  mktime(&tmJan4);
+  int jan4IsoWeekday = (tmJan4.tm_wday == 0) ? 7 : tmJan4.tm_wday;
+  
+  // Find the Monday of the week containing January 4 (the start of week 1).
+  time_t t_monday = mktime(&tmJan4) - (jan4IsoWeekday - 1) * 86400;
+  
+  // Compute the week number as the number of whole weeks between t_monday and t_thursday.
+  int weekNumber = 1 + ((int)(t_thursday - t_monday) / 86400) / 7;
+  return weekNumber;
+}
+
+
+void drawClock(int year, int month, int day, int week, const char* weekday, int hour, int minute, int second, const char* timezone){
   char buffer[256];
 
   u8g2_l.clearBuffer();
@@ -15,7 +64,7 @@ void drawClock(int year, int month, int day, int week, char* weekday, int hour, 
   u8g2_l.drawStr(0,64,buffer);
 
   // Left screen, top line, small: ISO week - Weekeday
-  sprintf(buffer, "W%02D %.16s", week%100, weekday);
+  sprintf(buffer, "W%02d %.16s", week, weekday);
   u8g2_l.setFont(u8g2_font_logisoso16_tr);
   u8g2_l.drawStr(0,20,buffer);
 
@@ -36,21 +85,42 @@ void drawClock(int year, int month, int day, int week, char* weekday, int hour, 
   // Right screen, top line, above :SS, small: timezone
   sprintf(buffer, "%.4s", timezone);
   u8g2_r.setFont(u8g2_font_logisoso16_tr);
-  u8g2_r.drawStr(5*36+30+5*(strlen(timezone)%2),16,buffer);
+  u8g2_r.drawStr(5*36+26+5*(strlen(timezone)%2),16,buffer);
 
   u8g2_r.sendBuffer();
 }
+
 
 void setup() {
   Serial.begin(9600);
   SPI.setFrequency(15000000);
   u8g2_l.begin();
   u8g2_r.begin();
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting ...");
+    delay(500);
+    }
+  Serial.println("Connected");  
+  configTime(0, 0, "pool.ntp.org");
+  struct tm timeinfo;
+  while (!getLocalTime(&timeinfo)) {
+    Serial.println("Waiting for time sync...");
+    delay(1000);
+  }
 }
 
+String TZ="Europe/Warsaw"; //to be controlled later
 void loop() {
-  drawClock(2025,3,20,12,"Wednesday", 21,37,12, "UTC");
-  sleep(2);
-  drawClock(2025,12,1,1,"Friday", 11,11,11, "CEST");
-  sleep(2);
+  time_t now = time(nullptr);
+  LocalDateTime ldt = LocalDateTime::forUnixSeconds64(now);
+  
+  TimeZone tz = manager.createForZoneName(TZ.c_str());
+  ZonedExtra ze = tz.getZonedExtra(ldt);
+  ZonedDateTime zdt = ZonedDateTime::forUnixSeconds64(now, tz);
+
+  int isoWeekNumber = computeIsoWeekNumber(zdt.year(),  zdt.month(), zdt.day());
+  drawClock(zdt.year(), zdt.month(), zdt.day(), isoWeekNumber, DateStrings().dayOfWeekLongString(zdt.dayOfWeek()), zdt.hour(), zdt.minute(), zdt.second(), ze.abbrev());
+
+  sleep(0.5);
 }
